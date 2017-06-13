191e2107f3244bf5838f1e33016dfac42fcbd34e
Eylam Fried <=>
2017-06-13 02:18:57 -0400

    solving

--- assignment/app.js
+++ assignment/app.js
@@ -3,7 +3,19 @@
  */
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/webdev_summer1_2017');
+//mongoose.connect('mongodb://127.0.0.1:27017/webdev_summer1_2017');
+
+
+var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer1_2017';
+
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds139801.mlab.com:39801/heroku_9hsqv3gp'; // user yours
+}
+
+mongoose.connect(connectionString);
 
 module.exports = function(app) {
     require('./services/user.service.server')(app);
